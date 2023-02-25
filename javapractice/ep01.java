a0688908310910c56fa86f39d69a29d55da9b2d4
onoderaryo <ryo.program.school@gmail.com>
2023-02-25 20:53:10 +0900

    package&JavaAPI

---
+++ javapractice/ep01.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+public class ep01 {
+	public static void main(String[] args) {
+		String str = "Javaの学習";
+		System.out.println(str);
+		System.out.println(str.length());
+		System.out.println(str.indexOf("学習"));
+		System.out.println(str.contains("Java"));
+		System.out.println(str.contains("Ruby"));
+		System.out.println(str.replace("Java", "Java言語"));
+
+	}
+}
