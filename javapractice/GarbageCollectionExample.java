a6a1fad7f94eb244767ededd57627067e1fb6f16
onoderaryo <ryo.program.school@gmail.com>
2023-03-02 16:40:51 +0900

    Chapter4_finish

--- javapractice/GarbageCollectionExample.java
+++ javapractice/GarbageCollectionExample.java
@@ -1,15 +1,16 @@
 package javapractice;
 
-class DataSet {
-	int[] data = new int[100];
+class DataSet2 {
+	int[] data = new int[1000];
 }
 
-public class FreeMoney {
+public class GarbageCollectionExample {
 	public static void main(String[] args) {
 		System.out.println("空きメモリサイズ" + Runtime.getRuntime().freeMemory());
 		DataSet[] data = new DataSet[10000];
 		for (int i = 0; i < 10000; i++) {
 			data[i] = new DataSet();
+			data[i] = null;
 			if ((i + 1) % 100 == 0) {
 				System.out.println("生成済みインスタンス数:" + (i + 1) + "空きメモリサイズ:" + Runtime.getRuntime().freeMemory());
 			}
