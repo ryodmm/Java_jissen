3a799e6412f9dbebf530795aba8a49b89c0247d5
onoderaryo <ryo.program.school@gmail.com>
2023-03-06 22:30:42 +0900

    Chapter5-7

---
+++ javapractice/EnchantedForExample.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class EnchantedForExample {
+	public static void main(String[] args) {
+		String[] month = { "Jan", "Feb", "Mar", "Apr", "May", "Jun" };
+		for (String str : month) {
+			System.out.println(str);
+		}
+	}
+
+}
