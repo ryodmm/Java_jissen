ebe4754e1144133645c6e4d7f62353a27375ead7
onoderaryo <ryo.program.school@gmail.com>
2023-03-07 16:29:55 +0900

    Chapter5-9

---
+++ javapractice/StackExample.java
@@ -0,0 +1,27 @@
+package javapractice;
+
+import java.util.LinkedList;
+
+public class StackExample {
+	public static void main(String[] args) {
+		LinkedList<String> stack = new LinkedList<>();
+
+		stack.push("(1)");
+		System.out.println("スタックの状態:" + stack);
+
+		stack.push("(2)");
+		System.out.println("スタックの状態:" + stack);
+
+		stack.push("(3)");
+		System.out.println("スタックの状態:" + stack);
+
+		stack.push("(4)");
+		System.out.println("スタックの状態:" + stack);
+
+		while (!stack.isEmpty()) {
+			System.out.println("要素の取り出し" + stack.pop());
+			System.out.println("スタックの状態" + stack);
+		}
+	}
+
+}
