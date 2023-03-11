fbc3e8576f4b12d9d6ecbfe7b252008f9ed11734
onoderaryo <ryo.program.school@gmail.com>
2023-03-11 20:20:30 +0900

    Chapter7-6

---
+++ javapractice/FileReadExample.java
@@ -0,0 +1,23 @@
+package javapractice;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadExample {
+	public static void main(String[] args) {
+		try {
+			File file = new File("/Users/onoderaryo/Desktop/Java/test.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String s;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s + "を読み込みました");
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
