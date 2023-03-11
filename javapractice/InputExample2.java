c2f2b49057e5b7004a7ceb0f50078c9d211f977d
onoderaryo <ryo.program.school@gmail.com>
2023-03-11 18:44:54 +0900

    Chapter7-2

---
+++ javapractice/InputExample2.java
@@ -0,0 +1,20 @@
+package javapractice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputExample2 {
+	public static void main(String[] args) {
+		InputStreamReader in = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(in);
+		try {
+			String line = reader.readLine();
+			double val = Double.parseDouble(line);
+			System.out.println("入力された値の平方根は" + Math.sqrt(val));
+			reader.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
